431085b0997f3de19135761b369a331cad09b1c6
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-11-29 18:20:26 +0100

    add redirecting

--- assigment1/app.js
+++ assigment1/app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   const method = req.method;
   if (url === "/") {
     res.write("<html>");
-    res.write("<head><title>mAIN PAGE</title><head>");
+    res.write("<head><title>MAIN PAGE</title><head>");
     res.write("<p>Hello!</p>");
     res.write(
       '<body><form action="/create-user" method="POST"><input type="text" name="create-user"><button type="submit">ADD USER</button></form></body>'
@@ -20,17 +20,16 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/create-user" && method === "POST") {
-    res.write("<head><title>CREATE USER</title><head>");
     let body = "";
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
-    return req.on("end", () => {
+    req.on("end", () => {
       console.log(body.split("=")[1]);
-      return res.end();
     });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
   }
-  res.write("<html><p>...other page</p></html>");
-  res.end();
 });
 server.listen(3000);
