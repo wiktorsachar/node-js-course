c5d78e2b6cbc8ce250b2c4fb548040e593f64d39
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-12-04 22:31:39 +0100

    add posting comment functionality

--- exercise-comment-list-pug/routes/basicRoutes.js
+++ exercise-comment-list-pug/routes/basicRoutes.js
@@ -2,15 +2,24 @@ const express = require("express");
 const router = express.Router();
 const comments = [];
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { comments });
 });
 router.get("/write-comment", (req, res) => {
   res.render("write-comment");
 });
 router.post("/send-post", (req, res) => {
-  const { comment, author } = req.body;
-  comments.push({ comment, author });
-  console.log(comment, author);
+  const date = new Date();
+  const time =
+    date.toLocaleDateString() +
+    " " +
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds();
+  const { title, comment, author } = req.body;
+  comments.push({ title, comment, author, time });
+  console.log(title, comment, author);
   res.redirect("/");
 });
 
