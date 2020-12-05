b435c07c53661e1d621b4680936a627fb726fbc0
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-12-05 14:32:00 +0100

    add index.pug styling

--- exercise-comment-list-pug/app.js
+++ exercise-comment-list-pug/app.js
@@ -1,7 +1,9 @@
+const path = require('path')
 const express = require("express");
 const routerData = require('./routes/basicRoutes')
 const bodyParser = require('body-parser');
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'pug');
