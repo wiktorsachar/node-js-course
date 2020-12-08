3eb67122bfd8c207022c70f2966ec532e1760935
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-12-08 16:39:37 +0100

    add .handlebars files

--- exercise-comment-list-handlebars/app.js
+++ exercise-comment-list-handlebars/app.js
@@ -1,11 +1,16 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
-const routerData = require('./routes/basicRoutes')
-const bodyParser = require('body-parser');
+const routerData = require("./routes/basicRoutes");
+const bodyParser = require("body-parser");
+const exphbs = require('express-handlebars');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
-app.set('view engine', 'pug');
-app.set('views', 'views');
-app.use(routerData.router)
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set("views", "views");
+app.use(routerData.router);
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 app.listen(3000);
