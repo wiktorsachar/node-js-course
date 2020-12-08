86804e534b7503a5187a98daaa6f9a9d0fd44a44
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-12-08 15:48:56 +0100

    add error 404 page

--- exercise-comment-list-pug/app.js
+++ exercise-comment-list-pug/app.js
@@ -1,11 +1,14 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
-const routerData = require('./routes/basicRoutes')
-const bodyParser = require('body-parser');
+const routerData = require("./routes/basicRoutes");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
-app.set('view engine', 'pug');
-app.set('views', 'views');
-app.use(routerData.router)
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "pug");
+app.set("views", "views");
+app.use(routerData.router);
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 app.listen(3000);
