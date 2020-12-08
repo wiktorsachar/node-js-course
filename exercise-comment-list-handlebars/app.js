67e176a9c00c47b7a491d50bd9d1861968493530
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-12-08 15:54:13 +0100

    add exercise skeleton from it's pug version

---
+++ exercise-comment-list-handlebars/app.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const express = require("express");
+const routerData = require('./routes/basicRoutes')
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(routerData.router)
+app.listen(3000);
