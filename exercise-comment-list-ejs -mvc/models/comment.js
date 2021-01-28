56cf4c62ac50830ad613d535e040f4ba8ece91af
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2021-01-28 17:03:05 +0100

    working on the new data storage and the model refactor

--- exercise-comment-list-ejs -mvc/models/comment.js
+++ exercise-comment-list-ejs -mvc/models/comment.js
@@ -1,5 +1,7 @@
+const fs = require("fs");
+
 const getTimeStamp = require("../util/timeStamp");
-const comments = [];
+// const comments = []; <-- previous version
 module.exports = class Comment {
   constructor(data) {
     this.title = data.title;
@@ -8,9 +10,24 @@ module.exports = class Comment {
     this.time = getTimeStamp();
   }
   save() {
+    //comments.push(this); <-- previous version
+    const comments = this.fetchComments();
     comments.push(this);
+    fs.writeFile(
+      "../data/comments.json",
+      JSON.stringify(comments),
+      (err, data) => {}
+    );
   }
   static fetchComments() {
-    return comments;
+    //return comments; <-- previous version
+    try {
+      const comments = fs.readFile("../data/comments.json", (err, data) => {
+        return JSON.parse(data);
+      });
+      return comments;
+    } catch {
+      return [];
+    }
   }
 };
